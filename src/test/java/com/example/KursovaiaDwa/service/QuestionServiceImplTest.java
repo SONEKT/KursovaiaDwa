4779b5242cc70c5747e17876ada698e26a357c17
SONEKT <shulmanlevgen@gmail.com>
2023-08-31 23:55:46 +0500

    Done

---
+++ src/test/java/com/example/KursovaiaDwa/service/QuestionServiceImplTest.java
@@ -0,0 +1,51 @@
+package com.example.KursovaiaDwa.service;
+
+import com.example.KursovaiaDwa.dto.Question;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuestionServiceImplTest {
+
+    QuestionServiceImpl underTest = new QuestionServiceImpl();
+
+    Question question1 = new Question("Q1", "A1");
+    Question question2 = new Question("Q2", "A2");
+
+    @Test
+    void add__questionAddedAndReturn() {
+        Question result = underTest.add(question1.getQuestion(), question1.getAnswer());
+
+        assertTrue(underTest.getAll().contains(question1));
+        assertEquals(question1, result);
+    }
+
+    @Test
+    void remove() {
+        underTest.add(question1.getQuestion(), question1.getAnswer());
+        Question result = underTest.remove(question1);
+
+        assertFalse(underTest.getAll().contains(question1));
+    }
+
+    @Test
+    void getAll_questionsAreInList_listReturned() {
+        underTest.add(question1);
+        underTest.add(question2);
+
+        Collection<Question> result = underTest.getAll();
+
+        assertEquals(List.of(question1, question2), result);
+    }
+
+    @Test
+    void getRandomQuestion() {
+        Question result = underTest.add(question1.getQuestion(), question1.getAnswer());
+
+        assertTrue(underTest.getAll().contains(question1));
+        assertEquals(question1, result);
+    }
+}
